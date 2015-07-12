c72cf5c64098e5cb8b6836f79a24420943543a1c
Pierre Borgmann <drweissbrot@outlook.com>
2015-07-12 19:47:43 +0200

    Make some links open in same tab

--- js/3-credits.js
+++ js/3-credits.js
@@ -1,21 +1,21 @@
-// PFUDOR.tk JavaScript Library -- Credits
+https:// PFUDOR.tk JavaScript Library -- Credits
 
 $(function(){
     if(pfudor == 'anim' || pfudor == 'huang') {
-        $('#credits').append("<a href=\"https://youtube.com/watch?v=eWM2joNb9NE\">Song by Andrew Huang</a><br/>");
+        $('#credits').append('<a href="https://youtube.com/watch?v=eWM2joNb9NE" target="_self">Song by Andrew Huang</a><br/>');
         if(pfudor == 'anim') {
-            $('#credits').append('<a href="https://youtube.com/watch?v=qRC4Vk6kisY">Video by FluffyMixer</a><br />');
-            $('#credits').append('<a href="//pfudor.tk?v=huang">Switch to the official video</a><br />');
+            $('#credits').append('<a href="https://youtube.com/watch?v=qRC4Vk6kisY" target="_self">Video by FluffyMixer</a><br />');
+            $('#credits').append('<a href="https://pfudor.tk?v=huang" target="_self">Switch to the official video</a><br />');
         } else {
-            $('#credits').append('<a href="//pfudor.tk?v=anim">Switch to the animated video</a><br />');
+            $('#credits').append('<a href="https://pfudor.tk?v=anim" target="_self">Switch to the animated video</a><br />');
         }
     }
 
     if(pfudor !== 'gaben') {
-        $('#credits').append('<a href="//pfudor.tk?v=gaben">Praise Lord Gaben!</a><br />');
+        $('#credits').append('<a href="https://pfudor.tk?v=gaben" target="_self">Praise Lord Gaben!</a><br />');
     }
 
     if(pfudor === false || pfudor == 'gaben') {
-        $('#credits').append('<a href="//youtube.com/watch?v=' + vidid + '">Switch to the video</a>');
+        $('#credits').append('<a href="https://youtube.com/watch?v=' + vidid + '" target="_self">Switch to the video</a>');
     }
 });
