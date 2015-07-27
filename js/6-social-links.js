20cdece074c8acaf608e4922894963c0bfeca669
Pierre Borgmann <drweissbrot@outlook.com>
2015-07-27 17:16:51 +0200

    Remake social links
    Social links are now dynamicly generated, based on which video (etc.) the user is on

---
+++ js/6-social-links.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#tw').attr('href', 'https://twitter.com/intent/tweet?text=PFUDOR.tk&amp;url=' + window.location);
+    $('#fb').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + window.location);
+    $('#gp').attr('href', 'https://plus.google.com/share?url=' + window.location);
+    $('#wa').attr('href', 'whatsapp://send?text=PFUDOR.tk%20' + window.location);
+});
